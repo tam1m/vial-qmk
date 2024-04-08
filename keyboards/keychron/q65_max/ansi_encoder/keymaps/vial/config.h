385ea8fd228febf4cbd6e98c278c2aab9e7da4f6
Adophoxia <andyao1528@gmail.com>
2024-04-07 21:19:34 -0700

    Add vial keymap

---
+++ keyboards/keychron/q65_max/ansi_encoder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x48, 0xB3, 0x3A, 0x21, 0xAD, 0xC2, 0x00, 0x47}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 14 }
